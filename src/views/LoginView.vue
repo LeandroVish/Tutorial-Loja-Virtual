042b0ba5a6fc6055c98603ebd53d4d4afdbcde56
LeandroVish <leandrogustavosantos.info@gmail.com>
2024-07-17 14:32:03 -0300

    skip pós aula perdida

---
+++ src/views/LoginView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import '@passageidentity/passage-elements/passage-auth';
+</script>
+
+<template>
+  <h1>Login</h1>
+  <div class="authContainer">
+    <passage-auth
+      app-id="MCgu7XgJFA80rxvUiMkPsuZp"
+    ></passage-auth>
+  </div>
+</template>
+
+<style></style>
